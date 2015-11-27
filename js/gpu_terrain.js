6d3be8f782f839fb2d30838d793fbb8c5c840cfc
Ben Williams <bww1@iastate.edu>
2015-11-27 12:33:40 -0600

    Basic vertex shader computation is working

--- js/gpu_terrain.js
+++ js/gpu_terrain.js
@@ -25,19 +25,31 @@ function main() {
 
     scene.add( camera );
 
-    // create a material for our plane		
+    // create a material for our plane -- 2^16 seeds
     material = new THREE.ShaderMaterial( {
+        uniforms: {
+                r: { 
+                    type: "f", 
+                    value: Math.random() * 1000
+                }
+            },
         vertexShader: document.getElementById( 'vertex_shader' ).textContent,
         fragmentShader: document.getElementById( 'fragment_shader' ).textContent
     } );
+
+    //material = new THREE.MeshNormalMaterial();
     
+    var geometry = new THREE.PlaneGeometry( 50, 50, 49, 49);
+
     // create a sphere and assign the material
     mesh = new THREE.Mesh( 
-        new THREE.IcosahedronGeometry( 20, 4 ), 
+        geometry, 
         material 
     );
     scene.add( mesh );
-    
+    mesh.rotation.x = -45;
+    mesh.rotation.z = 45;
+
     // create the renderer and attach it to the DOM
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
